c6cd222941396845a3a85e50a4a0dcb92fc57c27
Aswin-00 <aswingirish25@gmail.com>
2024-03-11 19:37:14 +0530

    uptoall

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('post/new/',post_new, name='post_new'),
+    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+
 
 ]
