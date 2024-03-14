4602aea7f96bed519838b00133f043cf6792d2c5
Aswin-00 <aswingirish25@gmail.com>
2024-03-14 15:31:46 +0530

    security_add

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('post/<pk>/publish/',post_publish, name='post_publish'),
+    path('post/<pk>/remove/',post_remove, name='post_remove'),
+
+    path('drafts/',post_draft_list, name='post_draft_list'),
     path('post/new/',post_new, name='post_new'),
     path('post/<int:pk>/edit/', post_edit, name='post_edit'),
     path('post/<int:pk>/', post_detail, name='post_detail'),
